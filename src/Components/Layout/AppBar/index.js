cf3fbe177f076243fabbf756fbcce3caf4e50d36
Olcay Kaplan <olcay.kaplan@datassist.com.tr>
2021-05-14 02:39:56 +0300

    link and routes added

--- src/Components/Layout/AppBar/index.js
+++ src/Components/Layout/AppBar/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+import About from '../../About';
+import MovieList from '../../MovieList';
+
 import {Grid, AppBar, Typography, Toolbar, Button, makeStyles, IconButton, MenuIcon,Box } from '@material-ui/core'
 
-import logo from  '../../../assets/img/logo.png'
 
 import classes from './index.module.css';
-
+import logo from '../../../assets/img/logo.png'
 
 const AppBarComponent = () =>{
 
 
   return(
-    <AppBar position="static" className={classes.menu}>
+    <AppBar  className={classes.menu}>
         <Toolbar className={classes.toolbar}>
           <img src={logo} alt="logo" className={classes.logo} />
           <Box>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">logout</Button>
+         
+          <Button color="inherit"  href="/about">About</Button>
+          <Button color="inherit" href="/movies">Movies</Button>
           </Box>
         </Toolbar>
       </AppBar>
--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import AppBar from './AppBar';
 import Footer from './Footer';
-import MovieList from '../MovieList'
-import {Grid, Paper} from '@material-ui/core';
 
-import classes from './index.module.css'
+import MovieList from '../MovieList'
+import MovieDetail from '../MovieDetail'
+import About from '../About'
+
+
+import {Grid, Paper} from '@material-ui/core';
+import { Redirect, Route, Switch } from 'react-router';
+
+import classes from './index.module.css';
 const Layout = () =>(
     <Grid className={classes.main}>
         <Grid container xs={12} sm={12} md={12} lg={12} xl={12} className={classes.header}>
             <AppBar/>
         </Grid>
-        <Grid container className={classes.content}>
-           <MovieList/>
+        <Grid container justify="center" className={classes.content}>
+          <Switch>
+          <Route path="/about" exact component={About} />
+          <Route path="/movies" exact component={MovieList} />
+          <Route path="/movies/:movieId" component={MovieDetail}/>
+          <Redirect to="/movies" exact />
+          </Switch>
         </Grid>
         <Grid item className={classes.footer}>
             <Footer/>
