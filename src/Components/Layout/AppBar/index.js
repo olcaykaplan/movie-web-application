985a7c58e6911438044bde828c332294ac5a76dc
Olcay Kaplan <olcay.kaplan@datassist.com.tr>
2021-05-16 18:56:00 +0300

    layout edited

--- src/Components/Layout/AppBar/index.js
+++ src/Components/Layout/AppBar/index.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import {Link} from 'react-router';
+import React from "react";
 
-import About from '../../About';
-import MovieList from '../../MovieList';
+import About from "../../About";
+import MovieList from "../../MovieList";
 
-import {Grid, AppBar, Typography, Toolbar, Button, makeStyles, IconButton, MenuIcon,Box } from '@material-ui/core'
+import {
+  Grid,
+  AppBar,
+  Typography,
+  Toolbar,
+  Button,
+  makeStyles,
+  IconButton,
+  MenuIcon,
+  Link,
+  Box,
+} from "@material-ui/core";
 
+import classes from "./index.module.css";
+import logo from "../../../assets/img/logo.png";
 
-import classes from './index.module.css';
-import logo from '../../../assets/img/logo.png'
-
-const AppBarComponent = () =>{
-
-
-  return(
-    <AppBar  className={classes.menu}>
-        <Toolbar className={classes.toolbar}>
-          <img src={logo} alt="logo" className={classes.logo} />
-          <Box>
-         
-          <Button color="inherit"  href="/about">About</Button>
-          <Button color="inherit" href="/movies">Movies</Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
-   
-) 
-}
+const AppBarComponent = () => {
+  return (
+    <AppBar className={classes.menu}>
+      <Toolbar className={classes.toolbar}>
+        <Link href="/movie">
+        <img src={logo} alt="logo" className={classes.logo} />      
+        </Link>
+        <Box>
+          <Button color="inherit" href="/about">
+            About
+          </Button>
+          <Button color="inherit" href="/movies">
+            Movies
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
 export default AppBarComponent;
