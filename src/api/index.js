858b30b2a9ba8dd6aaae9fc5cde49af846b142c2
Olcay Kaplan <olcay.kaplan@datassist.com.tr>
2021-05-16 18:52:58 +0300

    api const urls

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+export const baseUrl="https://api.themoviedb.org/3/movie/"
+export const apikey="?api_key=201644f743b0d24a770d286005c9c5b8"
+export const language="&language=en-US";
+export const imgBaseUrlPoster = "https://image.tmdb.org/t/p/w1280";
+export const imgBaseUrlDetail = "https://image.tmdb.org/t/p/original/"
+
+
